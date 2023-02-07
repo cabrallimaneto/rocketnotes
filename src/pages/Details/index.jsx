7f8b675bf20c29d09e22893ed9d714788d10d937
cabral <cabrallimacont@hotmail.com>
2023-02-06 21:07:22 -0300

    working in style

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,17 @@
 import { Container } from "./styles"
 
+import { Button } from "../../components/Button"
+
 export function Details () {
+
   return (
     <Container >
       <h1>Hello World!</h1>
       <span>Cabral lima</span>
+
+      <Button title="Entrar" loading />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
     </Container>
   )
 
