e1a339fb288c5166bced7ce78a6f0e06c083c6e0
cabral <cabrallimacont@hotmail.com>
2023-02-09 17:14:23 -0300

    create components input,note

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
-import { FiPlus} from "react-icons/fi"
+import { FiPlus, FiSearch } from "react-icons/fi"
 
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
+import { Note } from "./../../components/Note";
 import { Header } from "./../../components/Header";
+import { Input } from "./../../components/Input";
+import { Section } from "./../../components/Section";
 import { ButtonText } from "./../../components/ButtonText";
 
 export function Home() {
@@ -25,11 +28,21 @@ export function Home() {
       </Menu>
 
       <Search>
-
-
+        <Input placeholder="Pesquisar pelo título" />
       </Search>
 
       <Content>
+        <Section title="Minhas Notas">
+          <Note data={{
+            title:'React',
+            tags:[
+              { id:'1',name:'react' },
+              { id:'2',name:'rocketseat' }
+            ]
+          }}
+          />
+
+        </Section>
 
       </Content>
 
