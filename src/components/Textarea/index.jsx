f09461ab2e8bf05ce5ef94e4305478e38b1d741e
cabral <cabrallimacont@hotmail.com>
2023-03-08 16:37:44 -0300

    create components textarea, page.new

---
+++ src/components/Textarea/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from './styles';
+
+export function Textarea({value, ...rest} ) {
+
+  return (
+    <Container {...rest} >
+      {value}
+
+    </Container>
+
+
+  )
+
+}
---
+++ src/pages/New/index.jsx
@@ -0,0 +1,36 @@
+import { Header } from "./../../components/Header";
+import { Input } from "./../../components/Input";
+import { Textarea } from "./../../components/Textarea";
+
+
+import { Container, Form } from "./styles";
+
+export function New (){
+
+  return(
+
+    <Container>
+      <Header />
+
+      <main>
+        <Form >
+          <header>
+            <h1>Criar nota</h1>
+
+            <a href="/">voltar</a>
+
+          </header>
+
+            <Input placeholder="Titulo" />
+            <Textarea placeholder="observações" />
+
+        </Form>
+      </main>
+
+
+    </Container>
+
+
+  )
+
+}
